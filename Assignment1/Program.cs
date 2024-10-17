7d40f7e9e89cc9e7eeda8e1322e39bbe4828fc94
Okolochibuzo <okolovalentine25@gmail.com>
2024-10-17 15:58:26 +0100

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            String location = "";
+            DateTime time = DateTime.Now;
+
+
+            Console.WriteLine(location == null ? "location is null" : location);
+            Console.WriteLine(time == null ? "time is null" : time.ToString());
+            Console.ReadLine();
+
+            var AmaraDetails = new UserDetails();
+            AmaraDetails.FirstName = "Amara";
+            AmaraDetails.LastName = "Okolo";
+            AmaraDetails.PhoneNumber = 08037785881;
+            AmaraDetails.EmailAddress = "Amaraokolo@gmail.com";
+            AmaraDetails.Age = 21;
+
+            List  < string> ListOfNames = new List<string>();
+            ListOfNames.Add("Amara");
+            
+
+
+        }
+
+
+
+            
+
+
+    
+    }
+       
+        
+       
+
+    
+}
---
+++ Assignment1/UserDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class UserDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public float PhoneNumber { get; set; }
+        public float  PhoneNumberConfirmed { get; set; }
+        public DateTime Created { get; set; }
+        public int Age { get; set; }
+        public int Birthday { get; set; }
+       
+    }
+}
